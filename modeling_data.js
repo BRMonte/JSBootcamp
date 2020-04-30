e5dd20ee78c2fd6a0aeccbae1cc6d8c663ff7d33
bruno montebello <brunormontebello@hotmail.com>
2020-04-29 23:04:08 -0300

    Modeling data

---
+++ modeling_data.js
@@ -0,0 +1,45 @@
+let msg = "you are so grounded mr";
+msg.toUpperCase() // it does not reassign thr variable
+
+msg = msg.toUpperCase() // reassigns the variable
+
+let lala = "LALA";
+
+lala.toLowerCase();
+
+let color = "  purple    ";
+color.trim(); // cut the spaces only in the edges
+
+
+let tvShow = 'catdog';
+tvShow.indexOf('dog');
+
+
+let variable = "baseball"
+variable.slice(3); //eball
+variable.slice(2, 5); //seb
+
+let hello = "how are you my dear";
+hello.replace("dear", "angel"); //"how are you my angel"
+
+let templateLiterals = `hello, I'm ${30 + 2} years old`
+
+
+let loggedInUser = null // NULL is usefull for referencing this kind os situation
+
+Math.round(3,76); //3
+Math.pow(7, 2); //49
+Math.floor(7.56); //7
+Math.random(); //random value between 0 and 1
+
+const step1 = Math.random();
+const step2 = step1 * 10;
+
+Math.floor(Math.random() * 10) + 1 // proram to give random integer numbers
+
+let hey = "hello";
+typeof hey; // PS: TYPEOF OPERATOR USES NO PARENTHESIS
+
+parseInt("24") //24
+parseFloat("33.5") //33.5
+parseFloat("99cents") //99
