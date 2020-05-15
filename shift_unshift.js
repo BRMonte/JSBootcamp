f19281cdf9caf59387e83cda137e3dd5b0688d81
bruno montebello <brunormontebello@hotmail.com>
2020-05-14 22:33:09 -0300

    Add shift and unshift methods

---
+++ shift_unshift.js
@@ -0,0 +1,10 @@
+let hey = ["hello"]
+hey.unshift('Ciao')
+//2
+hey.unshift('Salut')
+//3
+hey
+//(3)[Salut, Ciao, hello]
+
+// shift remove item at the beginning
+// unshift add item at the begginning
