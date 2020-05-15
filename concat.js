ea78020a89d84f14a679f46a5fa2603ebd0d2329
bruno montebello <brunormontebello@hotmail.com>
2020-05-14 22:36:41 -0300

    Add concat method

---
+++ concat.js
@@ -0,0 +1,7 @@
+// use to merge two or more arrays into a new array
+// it does not change the originals array
+
+let hey = ["hey", "ciao"]
+let oi = ["salut", "ça va"]
+console.log(hey.concat(oi));
+//[hey, ciao, salut, ça va ]
