d39a40a146d04dcb920176d696f322d559ef545b
bruno montebello <brunormontebello@hotmail.com>
2020-05-16 10:41:54 -0300

    Add more functions technical elements

--- functions.js
+++ functions.js
@@ -6,3 +6,28 @@ function grumpus() {
   console.log('hey...');
   console.log('ho...');
 };
+
+function rollDice() {
+  let roll = Math.floor(Math.random() * 6) + 1;
+  console.log(`Rolled: ${roll}`);
+}
+
+function throuwDice() {
+  rollDice();
+  rollDice();
+  rollDice();
+  rollDice();
+  rollDice();
+}
+
+function square(num) {
+  console.log(num * num);
+}
+
+function sum(x, y) {
+  console.log( x + y)
+}
+
+function divide(a, b) {
+  console.log(a / b)
+}
