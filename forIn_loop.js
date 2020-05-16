5b2b581d2423678fb98aab657d38b86d89c52a0c
bruno montebello <brunormontebello@hotmail.com>
2020-05-16 10:26:56 -0300

    Add for in loop example

--- forIn_loop.js
+++ forIn_loop.js
@@ -1,2 +1,13 @@
 // loop over the keys of an object
 
+const hey = {
+  italian: "ciao",
+  spanish: "hola",
+  french: "salut",
+  brazilian: "e aí"
+};
+
+for (let i in hey) {
+  console.log(i); // i is the KEY not the ELEMENT nor the VALUE
+  console.log(hey[i]); // here we take the VALUE
+}
