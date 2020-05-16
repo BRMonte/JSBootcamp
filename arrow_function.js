bf7ee0c433f487c543701ce3f781c9fe7d12215d
bruno montebello <brunormontebello@hotmail.com>
2020-05-16 17:21:59 -0300

    Add arrow function example

---
+++ arrow_function.js
@@ -0,0 +1,14 @@
+// shorter way to write functions
+
+const times = (x, y) => { //only 2 parameters
+  return x * y;
+}
+
+const square = (x) => { //only 1 parameter
+  return x * x;
+}
+
+const square = () => { //no parameter
+  console.log('HI');
+}
+
