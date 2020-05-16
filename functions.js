ba0c36933aaf389d494052b519dfe6df51261815
bruno montebello <brunormontebello@hotmail.com>
2020-05-16 10:32:46 -0300

    Add function lecture

---
+++ functions.js
@@ -0,0 +1,8 @@
+// functions is a REUSABLE PROCEDURE
+// methods are FUNCTIONS: text.upperCase
+
+function grumpus() {
+  console.log('ola...');
+  console.log('hey...');
+  console.log('ho...');
+};
