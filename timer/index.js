0764ca2c3f8d1816d428ec19be7700cf581538ac
bruno montebello <brunormontebello@hotmail.com>
2020-05-21 13:48:56 -0300

    Add pause function

--- timer/index.js
+++ timer/index.js
@@ -12,11 +12,16 @@ class Timer {
     this.pauseButton = pauseButton;
 
     this.startButton.addEventListener('click', this.start);
+    this.pauseButton.addEventListener('click', this.pause);
   }
 
   start = () => { //this is written as an arrow function so THIS can return exactly what we need
     this.tick(); // this is being manually called before the interval starts runmning cause if not the Timer would wait 1000mls until starts
-    setInterval(this.tick, 1000); // serINterval is a built in method. The time is passed in milliseconms. 1000mls = 1s
+    this.interval = setInterval(this.tick, 1000); // serINterval is a built in method that gives us a timer. The time is passed in milliseconms. 1000mls = 1s. this.interval because we need to use interval in the PAUSE function
+  };
+
+  pause = () => {
+    clearInterval(this.interval); //another built in function that stops the timer
   };
 
   tick = () => {
