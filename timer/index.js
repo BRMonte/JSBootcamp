2dd21341c6b0878d23baf85676255fe81da01f2d
bruno montebello <brunormontebello@hotmail.com>
2020-05-22 01:06:27 -0300

    Add pause and tick function

--- timer/index.js
+++ timer/index.js
@@ -6,16 +6,25 @@
 //-> tick()
 
 class Timer {
-  constructor(durationInput, startButton, pauseButton){
+  constructor(durationInput, startButton, pauseButton, callbacks){
     this.durationInput = durationInput; // this lines creates an instance variable for duration input/start/pause, so it can be called from other scopes
     this.startButton = startButton;
     this.pauseButton = pauseButton;
+
+    if (callbacks) {
+      this.onStart = callbacks.onStart;
+    }
 
     this.startButton.addEventListener('click', this.start);
     this.pauseButton.addEventListener('click', this.pause);
   }
 
   start = () => { //this is written as an arrow function so THIS can return exactly what we need
+
+    if (this.onStart) {
+      this.onStart();
+    }
+
     this.tick(); // this is being manually called before the interval starts runmning cause if not the Timer would wait 1000mls until starts
     this.interval = setInterval(this.tick, 1000); // serINterval is a built in method that gives us a timer. The time is passed in milliseconms. 1000mls = 1s. this.interval because we need to use interval in the PAUSE function
   };
@@ -45,5 +54,17 @@ const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
-const timer = new Timer(durationInput, startButton, pauseButton); // this is an instance of the class Timer
-timer.start();
+const timer = new Timer(durationInput, startButton, pauseButton, {  // this is an instance of the class Timer
+  onStart() {
+    console.log('timer started');
+  },
+
+  onTick() {
+
+  },
+
+  onComplete() {
+
+  }
+
+});
