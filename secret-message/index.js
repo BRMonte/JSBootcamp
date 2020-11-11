9095866da26731ed5fb388d4c629223a5610bef6
Bruno Montebello <brunormontebello@hotmail.com>
2020-11-11 21:26:26 +0000

    Add things

--- secret-message/index.js
+++ secret-message/index.js
@@ -1,1 +1,15 @@
-console.log('Hi there');
+document.querySelector('form').addEventListener('submit', event => {
+  event.preventDefault();
+
+  document.querySelector('#message-form').classList.add('hide');
+  document.querySelector('#link-form').classList.remove('hide');
+
+  const input = document.querySelector('#message-input');
+  const encrypted = btoa(input.value);
+
+  document.querySelector(
+    '#link-input'
+  ).value = `${window.location}#${encrypted}`;
+  linkInput.select();
+
+});
